95946e8f2d4e9d8084123aa5d7b1c3a18bd66092
Ars Vladimirov <grokhael@gmail.com>
2023-07-20 19:45:21 +0600

    upd custom data

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,8 +1,8 @@
-**GC10-DET: Metallic Surface Defect Detection** is a dataset for object detection tasks. It is used in the manufacturing industry.
+**GC10-DET: Metallic Surface Defect Detection** is a dataset for object detection tasks. It is used in the industrial domain and defect detection research. 
 
 The dataset consists of 2300 images with 3563 labeled objects belonging to 10 different classes including *silk_spot*, *welding_line*, *punching_hole*, and other: *water_spot*, *crescent_gap*, *oil_spot*, *inclusion*, *waist folding*, *crease*, and *rolled_pit*.
 
-Each image in the GC10-DET dataset has bounding boxes annotations. There are 8 (0% of the total) unlabeled images (i.e. without annotations). There is 1 split in the dataset: *ds0* (2300 images). The dataset was released in 2020 by [The State Key Lab of Precision Measuring Technology and Instruments, Tianjin University, China](https://www.tsinghua.edu.cn/dpien/info/1092/1039.htm#:~:text=03%20Views%3A%20954-,The%20State%20Key%20Laboratory%20of%20Precision%20Measurement%20Technology%20and%20Instruments,to%20the%20public%20in%201995.).
+Images in the GC10-DET dataset have bounding box annotations. There are 8 (0% of the total) unlabeled images (i.e. without annotations). There are no pre-defined <i>train/val/test</i> splits in the dataset. The dataset was released in 2020 by the [The State Key Lab of Precision Measuring Technology and Instruments, Tianjin University, China](https://www.tsinghua.edu.cn/dpien/info/1092/1039.htm#:~:text=03%20Views%3A%20954-,The%20State%20Key%20Laboratory%20of%20Precision%20Measurement%20Technology%20and%20Instruments,to%20the%20public%20in%201995.).
 
 Here is the visualized example grid with annotations:
 
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,14 @@
 from typing import Dict, List, Optional, Union
 
-from dataset_tools.templates import AnnotationType, CVTask, Industry, License
+from dataset_tools.templates import (
+    AnnotationType,
+    Category,
+    CVTask,
+    Domain,
+    Industry,
+    License,
+    Research,
+)
 
 ##################################
 # * Before uploading to instance #
@@ -12,11 +20,19 @@ PROJECT_NAME_FULL: Optional[str] = "GC10-DET: Metallic Surface Defect Detection"
 # * After uploading to instance ##
 ##################################
 LICENSE: License = License.CC_BY_4_0()
-INDUSTRIES: List[Industry] = [Industry.Manufacturing()]
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [
+    Domain.Industrial(),
+    Research.DefectDetection(),
+]
+CATEGORY: Category = Category.Manufacturing()
+
 CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_YEAR: int = 2020
+RELEASE_DATE: Optional[str] = "2020-03-11"  # e.g. "YYYY-MM-DD"
+if RELEASE_DATE is None:
+    RELEASE_YEAR: int = None
+
 HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/alex000kim/gc10det"
 # e.g. "https://some.com/dataset/homepage"
 
@@ -41,12 +57,16 @@ PAPER: Optional[
     str
 ] = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7146379/pdf/sensors-20-01562.pdf"
 CITATION_URL: Optional[str] = "https://www.kaggle.com/datasets/alex000kim/gc10det"
+AUTHORS: Optional[List[str]] = ["Xiaoming Lv", "Fajie Duan", "Jia-jia Jiang", "Xiao Fu", "Lin Gan"]
+
 ORGANIZATION_NAME: Optional[
     Union[str, List[str]]
 ] = "The State Key Lab of Precision Measuring Technology and Instruments, Tianjin University, China"
 ORGANIZATION_URL: Optional[
     Union[str, List[str]]
 ] = r"https://www.tsinghua.edu.cn/dpien/info/1092/1039.htm#:~:text=03%20Views%3A%20954-,The%20State%20Key%20Laboratory%20of%20Precision%20Measurement%20Technology%20and%20Instruments,to%20the%20public%20in%201995."
+
+SLYTAGSPLIT: Optional[Dict[str, List[str]]] = None
 TAGS: List[str] = None
 
 ##################################
@@ -61,10 +81,15 @@ def check_names():
 
 
 def get_settings():
+    if RELEASE_DATE is not None:
+        global RELEASE_YEAR
+        RELEASE_YEAR = int(RELEASE_DATE.split("-")[0])
+
     settings = {
         "project_name": PROJECT_NAME,
         "license": LICENSE,
-        "industries": INDUSTRIES,
+        "applications": APPLICATIONS,
+        "category": CATEGORY,
         "cv_tasks": CV_TASKS,
         "annotation_types": ANNOTATION_TYPES,
         "release_year": RELEASE_YEAR,
@@ -76,13 +101,16 @@ def get_settings():
     if any([field is None for field in settings.values()]):
         raise ValueError("Please fill all fields in settings.py after uploading to instance.")
 
-    settings["project_name_full"] = PROJECT_NAME_FULL
+    settings["release_date"] = RELEASE_DATE
+    settings["project_name_full"] = PROJECT_NAME_FULL or PROJECT_NAME
     settings["download_original_url"] = DOWNLOAD_ORIGINAL_URL
     settings["class2color"] = CLASS2COLOR
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
+    settings["authors"] = AUTHORS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
-    settings["tags"] = TAGS if TAGS is not None else []
+    settings["slytagsplit"] = SLYTAGSPLIT
+    settings["tags"] = TAGS
 
     return settings
